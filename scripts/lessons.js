3a8a2c561b75975eccf3b27294c92857c79250d1
Ahmed Sami <ahmedsami372014@gmail.com>
2026-06-12 14:37:42 +0300

    commit

---
+++ scripts/lessons.js
@@ -0,0 +1,54 @@
+const lessonId = new URLSearchParams(window.location.search).get("id");
+
+const lessons = [
+  {
+    id: 1,
+    title: "The Introduction",
+    content: "",
+  },
+
+  {
+    id: 2,
+    title: "Strong Passwords",
+    content: "",
+  },
+
+  {
+    id: 3,
+    title: "Phishing Awareness",
+    content: "",
+  },
+];
+
+const {
+  data: { user },
+} = await supabase.auth.getUser();
+
+async function completeLessonFunc() {
+  const { error } = await supabase.from("lesson_progress").upsert({
+    user_id: user.id,
+    lesson_id: Number(lessonId),
+    completed: true,
+    completed_at: new Date(),
+  });
+
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("Lesson saved successfully");
+  }
+}
+
+const lesson = lessons.find((lesson) => lesson.id == lessonId);
+
+const completeLessonBtn = document.getElementById("completeLessonBtn");
+
+const lessonTitle = document.getElementById("lessonTitle");
+
+const lessonContent = document.getElementById("lessonContent");
+
+lessonTitle.textContent = lesson.title;
+
+lessonContent.textContent = lesson.content;
+
+completeLessonBtn.addEventListener("click", completeLessonFunc);
